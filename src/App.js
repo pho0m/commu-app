17e6a20b9f152f9fe63a810593de3a563f297840
pho0m <pho0m2000s@gmail.com>
2022-05-20 03:25:08 +0700

    feat(setup): init for commu app

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
-import { auth } from "./firebase_config";
-import { signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
-import { useState, useEffect } from "react";
 import Home from "./Home";
-import Header from "./Header";
 
 const App = () => {
-  const [userInfo, setUserInfo] = useState();
-
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      if (user) {
-        // User is logged in
-        //console.log("onAuth", user);
-        setUserInfo(user);
-      } else {
-        //  User is not logged in
-        setUserInfo(null);
-      }
-    });
-  }, []);
-
-  const login = () => {
-    const provider = new GoogleAuthProvider();
-    auth.languageCode = "th";
-    //auth.useDeviceLanguage();
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        //console.log(result.user); // We don’t really do it
-      })
-      .catch((error) => {
-        window.alert(error);
-      });
-  };
-
-  const logout = () => {
-    signOut(auth)
-      .then(() => {
-        // Sign-out successful.
-      })
-      .catch((error) => {
-        // An error happened.
-        alert(error);
-      });
-  };
-
   return (
-    <div className="container">
-      <Header user={userInfo} login={login} logout={logout} />
-      <Home user={userInfo} logout={logout} />
-    </div>
+    <>
+      <Home />
+    </>
   );
 };
+
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,5 @@
-import { useState } from "react";
-import Upload from "./Upload";
-
-const Home = (props) => {
-  const [usr, setUsr] = useState(null);
-
-  return (
-    <div className="text-center my-5 mx-auto w-50">
-      {props.user ? ( // user is logged in.
-        <div
-          style={{
-            border: "solid 1px green",
-          }}
-        >
-          <h2 className="my-3 bg-light">Home Page</h2>
-          <div>
-            <img src={props.user.photoURL} alt="user" />
-          </div>
-          <h4 className="bg-light text-secondary my-2 py-2">
-            {props.user.displayName}
-          </h4>
-          <Upload />
-        </div>
-      ) : (
-        // user is not logged in.
-        <h3 className="text-secondary">
-          You are not logged in. If you want to Upload Please login first!!!
-        </h3>
-      )}
-    </div>
-  );
+const Header = (props) => {
+  return <div>hellow world</div>;
 };
-export default Home;
+
+export default Header;
